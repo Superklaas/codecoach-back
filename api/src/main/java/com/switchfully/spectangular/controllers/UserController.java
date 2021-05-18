34408559de315a7d1237e760ce34e91ba775c227
Ann Cornelis <cornelis.ann@hotmail.com>
2021-05-18 11:21:57 +0200

    logger implementation in progress

--- api/src/main/java/com/switchfully/spectangular/controllers/UserController.java
+++ api/src/main/java/com/switchfully/spectangular/controllers/UserController.java
@@ -3,16 +3,21 @@ package com.switchfully.spectangular.controllers;
 import com.switchfully.spectangular.dtos.CreateUserDto;
 import com.switchfully.spectangular.dtos.UserDto;
 import com.switchfully.spectangular.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+
 
 @RestController
 @RequestMapping(path = "users")
 public class UserController {
 
     private final UserService userService;
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     public UserController(UserService userService) {
@@ -22,6 +27,7 @@ public class UserController {
     @PostMapping(consumes = "application/json", produces = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
     public UserDto createUser(@RequestBody CreateUserDto createUserDto) {
+        logger.info("Received request to create a new User.");
         return userService.createUser(createUserDto);
     }
 
@@ -29,6 +35,7 @@ public class UserController {
     @GetMapping(path = "/{id}", produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
     public UserDto getUserById(@PathVariable int id) {
+        logger.info("Received request to find a User by ")
         return userService.findUserById(id);
     }
 
