d5543a0c0ab6e9017a45a34a646dcfe3b447a7e4
Astrid <astrid.pinckers@minfin.fed.be>
2021-05-19 13:34:25 +0200

    added V9 flyway

---
+++ application/src/main/resources/db/migration/V9__UPDATE_USER_DUMMY_DATA.sql
@@ -0,0 +1,27 @@
+UPDATE codecoach.app_user
+SET img_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJDzxE36EOHv1gy0bVqmwiM-gVYpIh2IHBMA&usqp=CAU'
+WHERE user_id = 5;
+
+UPDATE codecoach.app_user
+SET availability = 'Thursdays from 11:00 till 14:00.',
+    introduction = 'I''m Anakin. I hate sand. I love code.',
+    img_url      = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJ8BEqNoiH37DZDO2C4psJYeCNhfARnASZIg&usqp=CAU'
+WHERE user_id = 3;
+
+UPDATE codecoach.app_user
+SET img_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU0qYHMBOIBuKdg35sSKvy6ChjQeUMX0gFFA&usqp=CAU'
+WHERE user_id = 1;
+
+UPDATE codecoach.app_user
+SET availability = 'Mondays through Wednesdays from 16:00 till 17:30 and Fridays from 15:45 till 18:15.',
+    introduction = 'Hi, I''m Padmé. I''d like to grow into a position of leadership, and therefore would like to take up a mentor role and coach people who need it.',
+    img_url      = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9i0Nmy-xO1fxBmKifCxvRNs45C90SIy2jlA&usqp=CAU'
+WHERE user_id = 4;
+
+UPDATE codecoach.app_user
+SET img_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRtZNbRM9ID82ohX1WoEB8RHNAEONgWKdKKQ&usqp=CAU'
+WHERE user_id = 6;
+
+UPDATE codecoach.app_user
+SET img_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjoFJgK5MnI6OGXv1c2CQqIKKqr24IwaENsw&usqp=CAU'
+WHERE user_id = 2;
