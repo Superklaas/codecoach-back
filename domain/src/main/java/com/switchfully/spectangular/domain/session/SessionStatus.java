2262fe8ad06d68feb427d69d4077e469dcf3a9bd
BOL CHRISTOPHE <christophe.bol@minfin.fed.be>
2021-05-20 17:03:49 +0200

    add FINISHED to session status

--- domain/src/main/java/com/switchfully/spectangular/domain/session/SessionStatus.java
+++ domain/src/main/java/com/switchfully/spectangular/domain/session/SessionStatus.java
@@ -1,5 +1,5 @@
 package com.switchfully.spectangular.domain.session;
 
 public enum SessionStatus {
-    REQUESTED,ACCEPTED, WAITING_FEEDBACK, DONE, CANCELLED
+    REQUESTED,ACCEPTED, WAITING_FEEDBACK, DONE, CANCELLED, FINISHED
 }
