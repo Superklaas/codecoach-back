c03a69f2451db5eb09fbb399b4d0070e323a60b3
BOL CHRISTOPHE <christophe.bol@minfin.fed.be>
2021-05-25 11:59:01 +0200

    Moved association between Role and Feature from the Role to the Feature enum

--- domain/src/main/java/com/switchfully/spectangular/domain/Feature.java
+++ domain/src/main/java/com/switchfully/spectangular/domain/Feature.java
@@ -1,13 +1,28 @@
 package com.switchfully.spectangular.domain;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.switchfully.spectangular.domain.Role.*;
+
 public enum Feature {
 
-    GET_USER_INFORMATION,
-    BECOME_COACH,
-    GET_ALL_COACHES,
-    CREATE_SESSION,
-    GET_ALL_COACHING_SESSION,
-    GET_ALL_COACHEE_SESSIONS,
-    UPDATE_SESSION_STATUS
+    GET_USER_INFORMATION(ADMIN, COACH, COACHEE),
+    BECOME_COACH(ADMIN, COACH, COACHEE),
+    GET_ALL_COACHES(ADMIN, COACH, COACHEE),
+    CREATE_SESSION(ADMIN, COACH, COACHEE),
+    GET_ALL_COACHING_SESSION(ADMIN, COACH),
+    GET_ALL_COACHEE_SESSIONS(ADMIN, COACH, COACHEE),
+    UPDATE_SESSION_STATUS(ADMIN, COACH, COACHEE);
 
+    private final List<Role> roleList;
+
+    Feature(Role... roleList) {
+        this.roleList = Arrays.asList(roleList);
+    }
+
+    public static List<Feature> getForRole(Role role) {
+        return Arrays.stream(Feature.values()).filter(f -> f.roleList.contains(role)).collect(Collectors.toList());
+    }
 }
