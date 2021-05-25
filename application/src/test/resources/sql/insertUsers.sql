d83dced08ae8421ae931834d101c514ce95b5e63
Ann Cornelis <cornelis.ann@hotmail.com>
2021-05-25 16:58:33 +0200

    added e2e for reset password functionality

--- application/src/test/resources/sql/insertUsers.sql
+++ application/src/test/resources/sql/insertUsers.sql
@@ -1,5 +1,5 @@
-INSERT INTO app_user (user_id, first_name, last_name, email, profile_name, password, role)
-VALUES (1000, 'Test', 'McTestFace', 'test@spectangular.com', 'T3sty', '$2a$10$IZCN0xQsxP0VKgxm5MItu.Rl2dpD3PMI3j0UNcm89WyrShQX6iAUe', 'COACHEE');
+INSERT INTO app_user (user_id, first_name, last_name, email, profile_name, password, role, reset_token)
+VALUES (1000, 'Test', 'McTestFace', 'test@spectangular.com', 'T3sty', '$2a$10$IZCN0xQsxP0VKgxm5MItu.Rl2dpD3PMI3j0UNcm89WyrShQX6iAUe', 'COACHEE', 'cba90cb7-4ef5-4fcb-a4a1-6d7177ce75e8');
 INSERT INTO app_user (user_id, first_name, last_name, email, profile_name, password, role)
 VALUES (1001, 'Coach', 'McCoachFace', 'coach@spectangular.com', 'C0achy', '$2a$10$IZCN0xQsxP0VKgxm5MItu.Rl2dpD3PMI3j0UNcm89WyrShQX6iAUe', 'COACH');
 
