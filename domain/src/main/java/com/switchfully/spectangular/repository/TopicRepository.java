c190c89babaec158e51e04ac5a6ea4311ab6bb77
Astrid <astrid.pinckers@minfin.fed.be>
2021-06-02 14:04:46 +0200

    create a new endpoint to only get the topics that are assigned to a coach

--- domain/src/main/java/com/switchfully/spectangular/repository/TopicRepository.java
+++ domain/src/main/java/com/switchfully/spectangular/repository/TopicRepository.java
@@ -2,8 +2,14 @@ package com.switchfully.spectangular.repository;
 
 import com.switchfully.spectangular.domain.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 public interface TopicRepository extends JpaRepository<Topic, Integer> {
+
+    @Query(value = "SELECT * FROM topic WHERE name IN (select topic_name from user_topics)", nativeQuery = true)
+    List<Topic> findAllUsedTopics();
 
 }
