ecd8a63229f513828692b1e4462b4d19bfa91aed
Astrid <astrid.pinckers@minfin.fed.be>
2021-06-03 10:20:52 +0200

    changed sql statement to inner join so online topics in users with coach role get selected

--- domain/src/main/java/com/switchfully/spectangular/repository/TopicRepository.java
+++ domain/src/main/java/com/switchfully/spectangular/repository/TopicRepository.java
@@ -9,7 +9,8 @@ import java.util.List;
 
 public interface TopicRepository extends JpaRepository<Topic, Integer> {
 
-    @Query(value = "SELECT * FROM topic WHERE name IN (select topic_name from user_topics)", nativeQuery = true)
+    @Query(value = "SELECT * FROM topic WHERE name IN (select topic_name from user_topics right join app_user on user_topics.user_id = app_user.user_id where app_user.role = 'COACH')",
+            nativeQuery = true)
     List<Topic> findAllUsedTopics();
 
 }
